1f069bfe84e35e22af0832b74e071bbb98fa22f5
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-17 21:42:03 +0300

    edit app js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
+import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
-import About from './pages/About';
-import Error404 from './pages/Error404';
-import Education from './pages/Education';
-import Experience from './pages/Experience';
+import routes from './utils/routes';
 
+const router = createBrowserRouter(routes);
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-    errorElement: <Error404 />,
-  },
-  {
-    path: '/about',
-    element: <About />,
-    errorElement: <Error404 />,
-  },
-  {
-    path: '/education',
-    element: <Education />,
-    errorElement: <Error404 />,
-  },
-  {
-    path: '/experience',
-    element: <Experience />,
-    errorElement: <Error404 />,
-  },
-]);
 function App() {
   return (
     <RouterProvider router={router} />
